a1d594bb756044989ffba3bcb44885a12e3174e4
Renz Viloria <viloriarenz917@gmail.com>
2025-03-31 00:23:14 +0800

    First act

---
+++ Person.java
@@ -0,0 +1,46 @@
+/* Write a Java program to create a class called Person with private instance
+ * variables name, age, and country . Provide public getter and setter methods
+ * and modify these variables.*/
+
+public class Person {
+    private String name;
+    private int age;
+    private String country;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public static void main(String[] args) {
+        
+        Person info = new Person();
+        info.setName("Renz");
+        info.setAge(19);
+        info.setCountry("Philippines");
+
+        System.out.println("Person Info:");
+        System.out.println("Name: " + info.getName());
+        System.out.println("Age: " + info.getAge());
+        System.out.println("Country: " + info.getCountry());
+    }
+}
